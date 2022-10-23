e76d61c7be7ad385702cb5de4699d15b4d9e4345
Yuyu <yuchi@xfers.com>
2022-10-23 18:32:05 +0800

    feat: 🎸 Add atomic tests

--- automated-atomic-tests/cypress/integration/exercise.spec.js
+++ automated-atomic-tests/cypress/integration/exercise.spec.js
@@ -1,49 +1,48 @@
 /// <reference types="cypress" />
-import LoginPage from '../page-objects/LoginPage';
-import ProductsPage from '../page-objects/ProductsPage';
-import AppHeader from '../page-objects/AppHeaderPage';
-import { LOGIN_USERS, PAGES } from '../support/e2eConstants';
+import LoginPage from "../page-objects/LoginPage";
+import ProductsPage from "../page-objects/ProductsPage";
+import AppHeader from "../page-objects/AppHeaderPage";
+import { LOGIN_USERS, PAGES } from "../support/e2eConstants";
 
-describe('Exercise for automated atomic tests', () => {
-  context('Home page', ()=>{
+describe("Exercise for automated atomic tests", () => {
+  context("Home page", () => {
     //runs before each test
     beforeEach(() => {
       // Open the baseUrl that comes from cypress.json
-      cy.visit('');
+      cy.visit("");
       // it's important to clear session storage so that it doesn't persist
       // between tests
       cy.window().then((win) => {
-        win.sessionStorage.clear()
+        win.sessionStorage.clear();
       });
     });
-  
-    it('should be visible', () => {
-      /** Your code below */
-      //Use the LoginPage page to check that the `screen.should('be.visible')`
-      /** Your code above */
-    });
-  
-    it('allows UI login with a standard user', () => {
-      /** Your code below */
 
+    it("should be visible", () => {
+      //Use the LoginPage page to check that the `screen.should('be.visible')`
+      LoginPage.screen.should("be.visible");
+    });
+
+    it("allows UI login with a standard user", () => {
       //1. Use the LoginPage page to signIn(LOGIN_USERS.STANDARD)
+      LoginPage.signIn(LOGIN_USERS.STANDARD);
       //2. Now check that the ProductsPage.screen is visible. 💡 the same as the test above, only with a different page object
-      
-      /** Your code above */
+      ProductsPage.screen.should("be.visible");
     });
   });
 
-  // typically we would actually put this into a separate file like Producs.spec.js
+  // typically we would actually put this into a separate file like Products.spec.js
   // and have another context(). However, 2 contexts() exist only for demonstration
-  context('Products', () => {
-    it('should be added to cart without UI login', () => {
-      /** Your code below */
-
+  context("Products", () => {
+    it("should be added to cart without UI login", () => {
       //1. Use the setTestContext() to set the sessionStorage first
+      cy.setTestContext({
+        user: LOGIN_USERS.STANDARD,
+        path: PAGES.INVENTORY,
+      });
       //2. Use the ProductsPage to addItemToCart(0);
+      ProductsPage.addItemToCart(0);
       //3. Now AppHeader.cart should('have.text', '1');
-      
-      /** Your code above */
+      AppHeader.cart.should("have.text", "1");
     });
   });
 });
